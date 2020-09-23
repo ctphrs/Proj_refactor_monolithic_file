7b753d1868fd6162fb202131fb0d47860ccc57a0
danny <danny@danny-VirtualBox>
2020-09-22 23:30:38 -0400

    sorking

---
+++ includes/fileio.h
@@ -0,0 +1,8 @@
+
+#ifndef FILEIO_FUNC_H
+#define FILEIO_FUNC_H
+
+	int loadData();
+	int saveData();
+
+#endif /* FILEIO_FUNC_H */
--- src/proj_refactor_file.cpp
+++ src/proj_refactor_file.cpp
@@ -10,39 +10,14 @@
 #include <vector>
 using namespace std;
 
-//default files to read and write data
-const std::string SOURCE_FILE 	= "source_file.txt";
-const std::string RESULTS_FILE 	= "results_file.txt";
+#include "../includes/fileio.h"
+#include "../includes/constants.h"
+#include "../includes/utils.h"
 
-//default return types
-const int 			EMPTY			 				=  0;
-const int 			SUCCESS			 				=  0;
-const int 			FAIL							=  SUCCESS - 1;
-const int 			COULD_NOT_OPEN_FILE				=  SUCCESS - 2;
-const int 			NO_DATA_TO_WORK_ON				=  SUCCESS - 3;
-const int			UNIMPLEMENTED					=  SUCCESS - 4;
-const int			UNINITIALIZED					=  SUCCESS - 5;
-
-//some data structures
-enum SORT_ORDER { PROCESS_NUMBER, CPU_TIME, START_TIME };
-struct process{	int process_number=UNINITIALIZED;
-				int cpu_time=UNINITIALIZED;
-				int start_time=UNINITIALIZED;};
 
 //holds all the processes read in
 vector<process> myProcesses;
 
-//attempt to open file 'filename' and read in all data
-//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int loadData(const std::string filename, vector<process> &myProcesses){
-	return UNIMPLEMENTED;
-}
-
-//attempt to create or open file 'filename' to write all data to
-//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int saveData(const std::string filename, vector<process> &myProcesses){
-	return UNIMPLEMENTED;
-}
 
 //sorts vector inplace based on mySortOrder (inplace means the vector is modified)
 //returns nothing
